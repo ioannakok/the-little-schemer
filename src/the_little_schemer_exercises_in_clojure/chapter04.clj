915d370be7871f81052db38d23cacd6610f2abf7
ioannakok <ioannakok@hotmail.com>
2018-07-28 12:07:38 +0100

    add-list function

--- src/the_little_schemer_exercises_in_clojure/chapter04.clj
+++ src/the_little_schemer_exercises_in_clojure/chapter04.clj
@@ -1,5 +1,6 @@
 (in-ns 'the-little-schemer-exercises-in-clojure.core)
 
+;; 4. Numbers
 (defn add [a b]
   (if (zero? a)
     b
@@ -10,3 +11,15 @@
   (if (zero? b)
     a
     (recur (dec a) (dec b))))
+
+
+(defn add-list [list]
+  (loop [list list
+         acc 0]
+    (if (empty? list)
+      acc
+      (recur (rest list) (+ (first list) acc)))))
+
+
+
+
--- test/the_little_schemer_exercises_in_clojure/chapter04_test.clj
+++ test/the_little_schemer_exercises_in_clojure/chapter04_test.clj
@@ -1,8 +1,11 @@
 (in-ns 'the-little-schemer-exercises-in-clojure.core-test)
 
+;; 4. Numbers
+
 (deftest add-test
   (testing "Addition"
     (is (= (add 46 12) 58))))
+
 
 (deftest sub-test
   (testing "Subtraction"
@@ -11,3 +14,12 @@
       14 3 11
       17 9 8
       18 25 -7)))
+
+
+(deftest add-list-test
+  (testing "It builds a number by totalling all the numbers in a list"
+    (are [list expected]
+      (= (add-list list) expected)
+      '() 0
+      '(3 5 2 8) 18
+      '(15 6 7 12 3) 43)))
